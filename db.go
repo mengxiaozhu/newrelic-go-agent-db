e4300deb5e72cc0b45a54504cca03568c7bf9193
cocotyty <cocotyty@sina.com>
2017-09-21 15:19:56 +0800

    init

---
+++ db.go
@@ -0,0 +1,166 @@
+package dbagent
+
+import "database/sql/driver"
+import (
+	"context"
+	"github.com/newrelic/go-agent"
+)
+
+var _ driver.Driver = &WrapperDriver{}
+
+func New(driver driver.Driver, application newrelic.Application) *WrapperDriver {
+	return &WrapperDriver{
+		driver, application,
+	}
+}
+
+type WrapperDriver struct {
+	driver.Driver
+	application newrelic.Application
+}
+
+func (d *WrapperDriver) Open(name string) (driver.Conn, error) {
+	conn, err := d.Driver.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	if qc, ok := conn.(driver.QueryerContext); ok {
+		if q, ok := conn.(driver.Queryer); ok {
+			if ec, ok := conn.(driver.ExecerContext); ok {
+				if e, ok := conn.(driver.Execer); ok {
+					return &WrapperConnQueryerAndExecer{
+						WrapperConn:    WrapperConn{conn, d.application},
+						QueryerContext: qc,
+						ExecerContext:  ec,
+						Queryer:        q,
+						Execer:         e,
+					}, nil
+				}
+			}
+		}
+	}
+	return &WrapperConn{conn, d.application}, nil
+}
+
+type WrapperConnQueryerAndExecer struct {
+	WrapperConn
+	driver.Queryer
+	driver.QueryerContext
+	driver.Execer
+	driver.ExecerContext
+}
+
+func (w *WrapperConnQueryerAndExecer) Query(query string, args []driver.Value) (driver.Rows, error) {
+	txn := w.Application.StartTransaction(query, nil, nil)
+	defer txn.End()
+	rs, err := w.Queryer.Query(query, args)
+	if err != nil {
+		txn.NoticeError(err)
+	}
+	return rs, err
+}
+
+func (w *WrapperConnQueryerAndExecer) Exec(query string, args []driver.Value) (driver.Result, error) {
+	txn := w.Application.StartTransaction(query, nil, nil)
+	defer txn.End()
+	rs, err := w.Execer.Exec(query, args)
+	if err != nil {
+		txn.NoticeError(err)
+	}
+	return rs, err
+}
+
+func (w *WrapperConnQueryerAndExecer) QueryContext(ctx context.Context, query string, args []driver.NamedValue) (driver.Rows, error) {
+	txn := w.Application.StartTransaction(query, nil, nil)
+	defer txn.End()
+	rs, err := w.QueryerContext.QueryContext(ctx, query, args)
+	if err != nil {
+		txn.NoticeError(err)
+	}
+	return rs, err
+}
+
+func (w *WrapperConnQueryerAndExecer) ExecContext(ctx context.Context, query string, args []driver.NamedValue) (driver.Result, error) {
+	txn := w.Application.StartTransaction(query, nil, nil)
+	defer txn.End()
+	rs, err := w.ExecerContext.ExecContext(ctx, query, args)
+	txn.End()
+	if err != nil {
+		txn.NoticeError(err)
+	}
+	return rs, err
+}
+
+type WrapperConn struct {
+	driver.Conn
+	newrelic.Application
+}
+
+func (w *WrapperConn) Prepare(query string) (driver.Stmt, error) {
+	stmt, err := w.Conn.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+	if qs, ok := stmt.(driver.StmtQueryContext); ok {
+		if es, ok := stmt.(driver.StmtExecContext); ok {
+			return &ContextWrapperStmt{
+				WrapperStmt:      WrapperStmt{stmt, w.Application, query},
+				StmtQueryContext: qs,
+				StmtExecContext:  es,
+			}, nil
+		}
+	}
+	return &WrapperStmt{stmt, w.Application, query}, nil
+}
+
+type WrapperStmt struct {
+	driver.Stmt
+	newrelic.Application
+	query string
+}
+
+func (s *WrapperStmt) Exec(args []driver.Value) (driver.Result, error) {
+	txn := s.Application.StartTransaction(s.query, nil, nil)
+	defer txn.End()
+	r, e := s.Stmt.Exec(args)
+	if e != nil {
+		txn.NoticeError(e)
+	}
+	return r, e
+}
+
+func (s *WrapperStmt) Query(args []driver.Value) (driver.Rows, error) {
+	txn := s.Application.StartTransaction(s.query, nil, nil)
+	defer txn.End()
+	r, e := s.Stmt.Query(args)
+	if e != nil {
+		txn.NoticeError(e)
+	}
+	return r, e
+}
+
+type ContextWrapperStmt struct {
+	WrapperStmt
+	driver.StmtExecContext
+	driver.StmtQueryContext
+}
+
+func (s *ContextWrapperStmt) ExecContext(ctx context.Context, args []driver.NamedValue) (driver.Result, error) {
+	txn := s.Application.StartTransaction(s.query, nil, nil)
+	defer txn.End()
+	r, e := s.StmtExecContext.ExecContext(ctx, args)
+	if e != nil {
+		txn.NoticeError(e)
+	}
+	return r, e
+}
+
+func (s *ContextWrapperStmt) QueryContext(ctx context.Context, args []driver.NamedValue) (driver.Rows, error) {
+	txn := s.Application.StartTransaction(s.query, nil, nil)
+	defer txn.End()
+	r, e := s.StmtQueryContext.QueryContext(ctx, args)
+	if e != nil {
+		txn.NoticeError(e)
+	}
+	return r, e
+}
---
+++ db_test.go
@@ -0,0 +1,52 @@
+package dbagent
+
+import (
+	"database/sql"
+	"github.com/jmoiron/sqlx"
+	"github.com/mengxiaozhu/go-sqlmock"
+	"github.com/newrelic/go-agent"
+	"os"
+	"testing"
+	"time"
+)
+
+type TestObject struct {
+	Name string `db:"name"`
+	Age  int    `db:"age"`
+}
+
+func TestNew(t *testing.T) {
+	app, err := newrelic.NewApplication(newrelic.NewConfig(os.Getenv("app"), os.Getenv("license")))
+	if err != nil {
+		t.Fatal(err)
+	}
+	driver := New(sqlmock.Pool, app)
+	_, sm, err := sqlmock.New()
+	if err != nil {
+		panic(err)
+	}
+	sql.Register("proxy", driver)
+	db, err := sql.Open("proxy", "sqlmock_db_0")
+	if err != nil {
+		panic(err)
+	}
+	sm.ExpectBegin()
+	sm.ExpectQuery(`select.*`).
+		WillDelayFor(time.Second).
+		WillReturnRows(sqlmock.NewRows([]string{"name", "age"}).
+			AddRow("a", 1))
+	DBX := sqlx.NewDb(db, "mysql")
+	o := &TestObject{}
+	tx, err := DBX.Beginx()
+	if err != nil {
+		panic(err)
+	}
+	err = tx.Get(o, "select * from table")
+	if err != nil {
+		panic(err)
+	}
+	tx.Commit()
+	if o.Name != "a" {
+		t.Fatal(o.Name)
+	}
+}
